56e2a1d3946797086844744756092ca08875ce98
mniebisch <michael.niebisch89@gmail.com>
2023-03-27 18:24:21 +0200

    Add function to convert node indices to bool mask.

--- src/effective_octo_potato/graph_utils.py
+++ src/effective_octo_potato/graph_utils.py
@@ -6,6 +6,7 @@ __all__ = [
     "create_edge_index",
     "create_left_hand_edge_index",
     "create_node_indices",
+    "create_node_indices",
     "create_pose_edge_index",
     "create_right_hand_edge_index",
 ]
@@ -52,6 +53,13 @@ def create_node_indices() -> torch.Tensor:
     pose_indices = torch.arange(489, 522)
     right_hand_indices = torch.arange(522, 543)
     return torch.cat([left_hand_indices, pose_indices, right_hand_indices])
+
+
+def create_node_mask() -> torch.Tensor:
+    node_indices = create_node_indices()
+    node_mask = torch.zeros_like(node_indices, dtype=torch.bool)
+    node_mask[node_indices] = True
+    return node_mask
 
 
 def _create_hand_edge_index() -> torch.Tensor:
