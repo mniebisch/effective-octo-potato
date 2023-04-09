7c4f384b66cc7f8f48013fe70266b8e026f476ce
Michael Niebisch <michael.niebisch89@gmail.com>
2023-04-09 08:06:08 +0200

    Restructure tests.

--- tests/effective_octo_potato/test_graph_utils.py
+++ tests/effective_octo_potato/test_graph_utils.py
@@ -3,34 +3,33 @@ import torch
 from effective_octo_potato import graph_utils
 
 
-def test_three_nodes() -> None:
-    input_mask = torch.tensor([True, False, True])
-    input_edge_index = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
+class TestApplyNodeMaskToEdges:
+    def test_three_nodes(self) -> None:
+        input_mask = torch.tensor([True, False, True])
+        input_edge_index = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
 
-    expected = torch.tensor([[1, 0], [0, 1]])
+        expected = torch.tensor([[1, 0], [0, 1]])
 
-    output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
+        output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
 
-    torch.testing.assert_close(expected, output)
+        torch.testing.assert_close(expected, output)
 
+    def test_no_removal(self) -> None:
+        input_mask = torch.tensor([True, True, True])
+        input_edge_index = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
 
-def test_no_removal() -> None:
-    input_mask = torch.tensor([True, True, True])
-    input_edge_index = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
+        expected = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
 
-    expected = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
+        output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
 
-    output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
+        torch.testing.assert_close(expected, output)
 
-    torch.testing.assert_close(expected, output)
+    def test_isolated_removal(self) -> None:
+        input_mask = torch.tensor([True, False, True, True])
+        input_edge_index = torch.tensor([[0, 2], [2, 3], [3, 0], [0, 3], [3, 2]]).T
 
+        expected = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
 
-def test_isolated_removal() -> None:
-    input_mask = torch.tensor([True, False, True, True])
-    input_edge_index = torch.tensor([[0, 2], [2, 3], [3, 0], [0, 3], [3, 2]]).T
+        output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
 
-    expected = torch.tensor([[0, 1], [1, 2], [2, 0], [0, 2], [2, 1]]).T
-
-    output = graph_utils.apply_node_mask_to_edges(input_mask, input_edge_index)
-
-    torch.testing.assert_close(expected, output)
+        torch.testing.assert_close(expected, output)
